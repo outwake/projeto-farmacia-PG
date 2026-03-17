332a5ab5787b24d05dcc3d0f6f8046d4d5ae61b7
Lary <larissa.mendy@gmail.com>
2026-03-17 12:18:34 -0300

    👌 IMPROVE: Implemantação de Produto

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriaModule } from './categoria/categoria.module';
 import { Categoria } from './categoria/entities/categoria.entity';
+import { ProdutoModule } from './produto/produto.module';
+import { Produto } from './produto/entities/produto.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -13,10 +15,10 @@ import { Categoria } from './categoria/entities/categoria.entity';
     username: 'root',
     password: 'root',
     database: 'db_projeto_farmacia',
-    entities:[Categoria],
+    entities:[Categoria, Produto],
     synchronize: true,
   }),
-   CategoriaModule ],
+   CategoriaModule, ProdutoModule ],
 
 
   controllers: [AppController],
