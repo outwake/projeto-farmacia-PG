8a618f4a5c7a623179bb21df53c6efd0cf8ee3f4
Lary <larissa.mendy@gmail.com>
2026-03-17 13:43:55 -0300

    🔒 Security:

---
+++ src/auth/controllers/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from '../guard/local-auth.guard';
+import { UsuarioLogin } from './../entities/usuariologin.entity';
+import { AuthService } from '../services/auth.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Usuario')
+@Controller("/usuarios")
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @UseGuards(LocalAuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @Post('/logar')
+    login(@Body() usuario: UsuarioLogin): Promise<any> {
+        return this.authService.login(usuario);
+    }
+
+}
